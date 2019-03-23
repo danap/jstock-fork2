8d87907832d45bfb688049b8fb49e9de2b28e8d0
danap <danap@dandymadeproductions.com>
2019-03-23 13:38:50 -0600

    method doInBackground() Called the Utils.extractZipFile() Directly for Companies Stock Listing by Country Files Extraction Rather Than jstock.initPreloadDatabase(). Added Class Methods loadStockInfo/NameDatabaseFromCSV(). Added Method loadUserDefinedDatabaseFromCSV(). Added Class Instance ZIP_DATABASE_FILE_NAME.

--- src/org/yccheok/jstock/gui/DatabaseTask.java
+++ src/org/yccheok/jstock/gui/DatabaseTask.java
@@ -3,7 +3,7 @@
  * Copyright (C) 2016 Yan Cheng Cheok <yccheok@yahoo.com>
  * Copyright (C) 2019 Dana Proctor
  * 
- * Version 1.0.7.37.01 03/16/2019
+ * Version 1.0.7.37.02 03/23/2019
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -32,6 +32,12 @@
 //                                & Log to Constructor. Referenced Those Instances Throughout
 //                                to Replace Local References in Inner Class to JStock.
 //                                Formatted.
+//         1.0.7.37.02 03/23/2019 Method doInBackground() Called the Utils.extractZipFile()
+//                                Directly for Companies Stock Listing by Country Files
+//                                Extraction Rather Than jstock.initPreloadDatabase(). Added
+//                                Class Methods loadStockInfo/NameDatabaseFromCSV(). Added
+//                                Method loadUserDefinedDatabaseFromCSV(). Added Class Instance
+//                                ZIP_DATABASE_FILE_NAME.
 //
 //-----------------------------------------------------------------
 //                 yccheok@yahoo.com
@@ -42,19 +48,25 @@ package org.yccheok.jstock.gui;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.concurrent.CancellationException;
 
 import javax.swing.SwingWorker;
 
 import org.apache.commons.logging.Log;
+import org.yccheok.jstock.engine.Board;
 import org.yccheok.jstock.engine.Code;
 import org.yccheok.jstock.engine.Country;
+import org.yccheok.jstock.engine.Industry;
 import org.yccheok.jstock.engine.Pair;
 import org.yccheok.jstock.engine.Stock;
 import org.yccheok.jstock.engine.StockInfo;
 import org.yccheok.jstock.engine.StockInfoDatabase;
 import org.yccheok.jstock.engine.StockNameDatabase;
 import org.yccheok.jstock.engine.Symbol;
+import org.yccheok.jstock.file.Atom;
+import org.yccheok.jstock.file.Statement;
+import org.yccheok.jstock.file.Statements;
 import org.yccheok.jstock.internationalization.GUIBundle;
 
 //Task to initialize both stockInfoDatabase and stockNameDatabase.
@@ -62,7 +74,7 @@ import org.yccheok.jstock.internationalization.GUIBundle;
 /**
  * @author doraemon
  * @author Dana M. Proctor
- * @version 1.0.7.37.01 03/16/2019
+ * @version 1.0.7.37.02 03/23/2019
  */
 
 class DatabaseTask extends SwingWorker<Boolean, Void>
@@ -71,6 +83,8 @@ class DatabaseTask extends SwingWorker<Boolean, Void>
    private JStock jstock;
    private Log log;
    private boolean readFromDisk = true;
+   
+   public final static String ZIP_DATABASE_FILE_NAME = "database" + File.separator + "database.zip";
 
    protected DatabaseTask(JStock jstock, Log log, boolean readFromDisk)
    {
@@ -145,21 +159,21 @@ class DatabaseTask extends SwingWorker<Boolean, Void>
 
       if (this.readFromDisk)
       {
-         StockInfoDatabase tmp_stock_info_database = jstock.loadStockInfoDatabaseFromCSV(country);
+         StockInfoDatabase tmp_stock_info_database = loadStockInfoDatabaseFromCSV(country);
          if (tmp_stock_info_database == null)
          {
             // Perhaps we are having a corrupted database. We will
             // restore from database.zip.
-            jstock.initPreloadDatabase(true);
+            Utils.extractZipFile(ZIP_DATABASE_FILE_NAME, true);
 
-            tmp_stock_info_database = jstock.loadStockInfoDatabaseFromCSV(country);
+            tmp_stock_info_database = loadStockInfoDatabaseFromCSV(country);
          }
 
          // StockNameDatabase is an optional item.
          final StockNameDatabase tmp_name_database;
          if (org.yccheok.jstock.engine.Utils.isNameImmutable())
          {
-            tmp_name_database = jstock.loadStockNameDatabaseFromCSV(country);
+            tmp_name_database = loadStockNameDatabaseFromCSV(country);
          }
          else
          {
@@ -177,7 +191,7 @@ class DatabaseTask extends SwingWorker<Boolean, Void>
          {
             // Yes. We need to integrate "user-defined-database.csv" into
             // tmp_stock_info_database
-            final java.util.List<Pair<Code, Symbol>> pairs = jstock.loadUserDefinedDatabaseFromCSV(country);
+            final java.util.List<Pair<Code, Symbol>> pairs = loadUserDefinedDatabaseFromCSV(country);
 
             boolean addUserDefinedStockInfoSuccessAtLeastOnce = false;
 
@@ -276,7 +290,7 @@ class DatabaseTask extends SwingWorker<Boolean, Void>
 
             // Yes. We need to integrate "user-defined-database.csv" into
             // tmp_stock_info_database
-            final java.util.List<Pair<Code, Symbol>> pairs = jstock.loadUserDefinedDatabaseFromCSV(country);
+            final java.util.List<Pair<Code, Symbol>> pairs = loadUserDefinedDatabaseFromCSV(country);
 
             if (pairs.isEmpty() == false)
             {
@@ -318,5 +332,110 @@ class DatabaseTask extends SwingWorker<Boolean, Void>
          }
       }
       return false;
+   }
+   
+   private StockInfoDatabase loadStockInfoDatabaseFromCSV(Country country)
+   {
+      final File stockInfoDatabaseCSVFile = org.yccheok.jstock.engine.Utils.getStockInfoDatabaseFile(country);
+
+      Statements statements = Statements.newInstanceFromCSVFile(stockInfoDatabaseCSVFile);
+      
+      if (statements.getType() != Statement.Type.StockInfoDatabase)
+         return null;
+      
+      java.util.List<Stock> stocks = new ArrayList<Stock>();
+      
+      for (int i = 0, ei = statements.size(); i < ei; i++)
+      {
+         Statement statement = statements.get(i);
+         Atom atom0 = statement.getAtom(0);
+         Atom atom1 = statement.getAtom(1);
+         Atom atom2 = statement.getAtom(2);
+         Atom atom3 = statement.getAtom(3);
+
+         Code code = Code.newInstance(atom0.getValue().toString());
+         Symbol symbol = Symbol.newInstance(atom1.getValue().toString());
+         Industry industry = Industry.Unknown;
+         Board board = Board.Unknown;
+         
+         try
+         {
+            industry = Industry.valueOf(atom2.getValue().toString());
+         }
+         catch (Exception exp)
+         {
+            log.error(null, exp);
+         }
+         
+         try
+         {
+            board = Board.valueOf(atom3.getValue().toString());
+         }
+         catch (Exception exp)
+         {
+            log.error(null, exp);
+         }
+
+         Stock stock = Stock.builder(code, symbol).board(board).industry(industry).build();
+         stocks.add(stock);
+      }
+      return new StockInfoDatabase(stocks);
+   }
+   
+   private StockNameDatabase loadStockNameDatabaseFromCSV(Country country)
+   {
+      final File stockNameDatabaseCSVFile = new File(org.yccheok.jstock.gui.Utils.getUserDataDirectory()
+                                                     + country + File.separator + "database" + File.separator
+                                                     + "stock-name-database.csv");
+
+      Statements statements = Statements.newInstanceFromCSVFile(stockNameDatabaseCSVFile);
+      
+      if (statements.getType() != Statement.Type.StockNameDatabase)
+         return null;
+      
+      java.util.List<Stock> stocks = new ArrayList<Stock>();
+      
+      for (int i = 0, ei = statements.size(); i < ei; i++)
+      {
+         Statement statement = statements.get(i);
+         Atom atom0 = statement.getAtom(0);
+         Atom atom1 = statement.getAtom(1);
+         Code code = Code.newInstance(atom0.getValue().toString());
+         String name = atom1.getValue().toString();
+
+         // Symbol doesn't matter. Just provide a dummy value for it.
+         Stock stock = Stock.builder(code, Symbol.newInstance(code.toString())).name(name).build();
+         stocks.add(stock);
+      }
+      return new StockNameDatabase(stocks);
+   }
+   
+   private java.util.List<Pair<Code, Symbol>> loadUserDefinedDatabaseFromCSV(Country country)
+   {
+      final File userDefinedDatabaseCSVFile = new File(org.yccheok.jstock.gui.Utils.getUserDataDirectory()
+                                                       + country + File.separator + "database"
+                                                       + File.separator + "user-defined-database.csv");
+
+      if (!userDefinedDatabaseCSVFile.exists())
+         return new ArrayList<Pair<Code, Symbol>>();
+
+      Statements statements = Statements.newInstanceFromCSVFile(userDefinedDatabaseCSVFile);
+      
+      if (statements.getType() != Statement.Type.UserDefinedDatabase)
+         return new ArrayList<Pair<Code, Symbol>>();
+      
+      java.util.List<Pair<Code, Symbol>> pairs = new ArrayList<Pair<Code, Symbol>>();
+      
+      for (int i = 0, ei = statements.size(); i < ei; i++)
+      {
+         Statement statement = statements.get(i);
+         Atom atom0 = statement.getAtom(0);
+         Atom atom1 = statement.getAtom(1);
+         Code code = Code.newInstance(atom0.getValue().toString());
+         Symbol symbol = Symbol.newInstance(atom1.getValue().toString());
+
+         pairs.add(new Pair<Code, Symbol>(code, symbol));
+      }
+      return pairs;
    }
 }
