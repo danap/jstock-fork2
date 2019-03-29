d0275cef0420020e7a52b9f52d1466242f3428e7
danap <danap@dandymadeproductions.com>
2019-03-29 13:10:33 -0600

    Method createEmptyWatchlist() Used Replaced Class JSTock.CVSWatchList With New Class In This Package CSVWatchList. Same Method Call to WatchListJPanel saveCSVWatchList().

--- src/org/yccheok/jstock/watchlist/Utils.java
+++ src/org/yccheok/jstock/watchlist/Utils.java
@@ -1,6 +1,9 @@
 /*
  * JStock - Free Stock Market Software
  * Copyright (C) 2010 Yan Cheng CHEOK <yccheok@yahoo.com>
+ * Copyright (C) 2019 Dana Proctor
+ * 
+ * Version 1.0.7.37.30 03/28/2019
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,6 +19,22 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+//=================================================================
+// Revision History
+// Changes to the code should be documented here and reflected
+// in the present version number. Author information should
+// also be included with the original copyright author.
+//=================================================================
+//
+// Version 1.0.7.37    08/26/2018 Original Yan Cheng, WatchList Utils Class.
+//         1.0.7.37.01 03/29/2019 Method createEmptyWatchlist() Used Replaced Class
+//                     JStock.CVSWatchList With New Class In This Package CSVWatchList.
+//                     Same Method Call to WatchListJPanel saveCSVWatchList().
+//
+//-----------------------------------------------------------------
+//                 yccheok@yahoo.com
+//                 danap@dandymadeproductions.com
+//=================================================================
 
 package org.yccheok.jstock.watchlist;
 
@@ -33,6 +52,8 @@ import org.yccheok.jstock.gui.StockTableModel;
 /**
  *
  * @author yccheok
+ * @author Dana M. Proctor
+ * @version 1.0.7.37.01 03/29/2019
  */
 public class Utils {
     // Use ThreadLocal to ensure thread safety.
@@ -156,9 +177,11 @@ public class Utils {
 
         final StockTableModel stockTableModel = new StockTableModel();
         
-        JStock.CSVWatchlist csvWatchlist = JStock.CSVWatchlist.newInstance(stockTableModel);
+        //JStock.CSVWatchlist csvWatchlist = JStock.CSVWatchlist.newInstance(stockTableModel);
+        CSVWatchList csvWatchlist = new CSVWatchList(stockTableModel);
 
-        return JStock.saveCSVWatchlist(directory, csvWatchlist);
+        //return JStock.saveCSVWatchlist(directory, csvWatchlist);
+        return JStock.instance().getWatchListPanel().saveCSVWatchlist(directory, csvWatchlist);
     }
     
     public static List<WatchlistInfo> getWatchlistInfos() {
