d0effb93941b2c28dda2875bf73e83f608c1fde2
danap <danap@dandymadeproductions.com>
2019-04-08 18:14:23 -0600

    Method getRealTimeStockMonitorObserver() Changed Return Type From Observer<RealTimeStockMonitor, List<Stock>> to Observer<RealTimeStockMonitor, Result>>. Same Method Changed Throughout the Use of Result Rather Than List<Stock>. Commented Methods getDelay() & getTotalScanned().

--- src/org/yccheok/jstock/engine/RealTimeIndexMonitor.java
+++ src/org/yccheok/jstock/engine/RealTimeIndexMonitor.java
@@ -1,6 +1,9 @@
 /*
  * JStock - Free Stock Market Software
  * Copyright (C) 2014 Yan Cheng Cheok <yccheok@yahoo.com>
+ * Copyright (C) 2019 Dana Proctor
+ * 
+ * Version 1.0.7.37.01 04/08/2019
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,6 +19,24 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+//=================================================================
+// Revision History
+// Changes to the code should be documented here and reflected
+// in the present version number. Author information should
+// also be included with the original copyright author.
+//=================================================================
+//
+// Version 1.0.7.9     08/26/2018 Original Yan Cheng, JStock Engine RealTimeIndexMonitor Class.
+//         1.0.7.37.01 04/08/2019 Method getRealTimeStockMonitorObserver() Changed Return
+//                                Type From Observer<RealTimeStockMonitor, List<Stock>> to
+//                                Observer<RealTimeStockMonitor, Result>>. Same Method Changed
+//                                Throughout the Use of Result Rather Than List<Stock>. Commented
+//                                Methods getDelay() & getTotalScanned().
+//
+//-----------------------------------------------------------------
+//                 yccheok@yahoo.com
+//                 danap@dandymadeproductions.com
+//=================================================================
 
 package org.yccheok.jstock.engine;
 
@@ -24,9 +45,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.yccheok.jstock.engine.RealTimeStockMonitor.Result;
+
 /**
  *
  * @author yccheok
+ * @author Dana M. Proctor
+ * @version 1.0.7.37.01 04/08/2019
  */
 public class RealTimeIndexMonitor extends Subject<RealTimeIndexMonitor, java.util.List<Market>> {
     /** Creates a new instance of RealTimeIndexMonitor */
@@ -36,14 +61,15 @@ public class RealTimeIndexMonitor extends Subject<RealTimeIndexMonitor, java.uti
         realTimeStockMonitor.attach(getRealTimeStockMonitorObserver());
     }
     
-    private Observer<RealTimeStockMonitor, List<Stock>> getRealTimeStockMonitorObserver() {
-        return new Observer<RealTimeStockMonitor, List<Stock>>() {
-
+    private Observer<RealTimeStockMonitor, Result> getRealTimeStockMonitorObserver() {
+       return new Observer<RealTimeStockMonitor, Result>() {
+            
             @Override
-            public void update(RealTimeStockMonitor subject, List<Stock> stocks) {
+            public void update(RealTimeStockMonitor subject, Result result) {
                 
                 List<Market> markets = new ArrayList<Market>();
-                for (Stock stock : stocks) {
+                
+                for (Stock stock : result.stocks) {
                     Index index = indexMapping.get(stock.code);
                     if (index != null) {
                         markets.add(Market.newInstance(stock, index));
@@ -104,17 +130,23 @@ public class RealTimeIndexMonitor extends Subject<RealTimeIndexMonitor, java.uti
         realTimeStockMonitor.refresh();
     }
     
+    // Not in 1.0.7.37 or private.
+    /*
     public synchronized long getDelay() {
         return realTimeStockMonitor.getDelay();
     }
+    */
     
     public synchronized void setDelay(int delay) {
         realTimeStockMonitor.setDelay(delay);
     }
     
+    // Not in 1.0.7.37 or private.
+    /*
     public int getTotalScanned() {
         return realTimeStockMonitor.getTotalScanned();
     }
+    */
     
     private final Map<Code, Index> indexMapping = new ConcurrentHashMap<Code, Index>();
     private final RealTimeStockMonitor realTimeStockMonitor;
