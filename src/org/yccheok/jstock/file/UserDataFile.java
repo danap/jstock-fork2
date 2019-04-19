f3fde636f82f6436ce651536b12285a9ef81094e
danap <danap@dandymadeproductions.com>
2019-04-19 12:29:57 -0600

    Changed Class Instance UIOptionsJson to UIOptionsXml. Also Changed String to uioptions.xml

--- src/org/yccheok/jstock/file/UserDataFile.java
+++ src/org/yccheok/jstock/file/UserDataFile.java
@@ -1,6 +1,9 @@
 /*
  * JStock - Free Stock Market Software
  * Copyright (C) 2018 Yan Cheng Cheok <yccheok@yahoo.com>
+ * Copyright (C) 2019 Dana Proctor
+ * 
+ * Version 1.0.7.37.01 04/19/2019
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,12 +19,29 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+//=================================================================
+// Revision History
+// Changes to the code should be documented here and reflected
+// in the present version number. Author information should
+// also be included with the original copyright author.
+//=================================================================
+//
+// Version 1.0.7.37    08/26/2018 Original Yan Cheng, JStock File UserDataFile Class.
+//         1.0.7.37.01 04/19/2019 Changed Class Instance UIOptionsJson to UIOptionsXml.
+//                                Also Changed String to uioptions.xml.
+//
+//-----------------------------------------------------------------
+//                 yccheok@yahoo.com
+//                 danap@dandymadeproductions.com
+//=================================================================
 
 package org.yccheok.jstock.file;
 
 /**
  *
  * @author yccheok
+ * @author Dana M. Proctor
+ * @version 1.0.7.37.01 04/19/2019
  */
 public enum UserDataFile {
     CashFlowChartJDialogXml("cashflowchartjdialog.xml"),
@@ -30,7 +50,7 @@ public enum UserDataFile {
     MainFrameXml("mainframe.xml"),
     OptionsXml("options.xml"),
     PortfolioManagementJPanelXml("portfoliomanagementjpanel.xml"),
-    UIOptionsJson("uioptions.json");
+    UIOptionsXml("uioptions.xml");
     
     UserDataFile(String filename) {
         this.filename = filename;
