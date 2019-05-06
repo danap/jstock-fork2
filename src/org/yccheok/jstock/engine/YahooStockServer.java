693d63d5d25e45622c4de4391814829469c5ccd6
danap <danap@dandymadeproductions.com>
2019-05-06 12:47:36 -0600

    Commented No Longer Used Method getYahooCSVBasedURL().

--- src/org/yccheok/jstock/engine/YahooStockServer.java
+++ src/org/yccheok/jstock/engine/YahooStockServer.java
@@ -1,6 +1,9 @@
 /*
  * JStock - Free Stock Market Software
  * Copyright (C) 2010 Yan Cheng CHEOK <yccheok@yahoo.com>
+ * Copyright (C) 2019 Dana Proctor
+ * 
+ * Version 1.0.7.9.01 05/6/2019
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,18 +19,39 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+//=================================================================
+// Revision History
+// Changes to the code should be documented here and reflected
+// in the present version number. Author information should
+// also be included with the original copyright author.
+//=================================================================
+//
+// Version 1.0.7.9     07/21/2015 Original Yan Cheng, JStock Engine YahooStockServer Class.
+//         1.0.7.9.01  04/06/2019 Commented No Longer Used Method getYahooCSVBasedURL().
+//
+//                                
+//-----------------------------------------------------------------
+//                 yccheok@yahoo.com
+//                 danap@dandymadeproductions.com
+//=================================================================
 
 package org.yccheok.jstock.engine;
 
 /**
  *
  * @author yccheok
+ * @author Dana M. Proctor
+ * @version 1.0.7.9.01 05/06/2019
  */
 public class YahooStockServer extends AbstractYahooStockServer {
+    
+    // In 1.0.7.9, removed a some point after.
+    /*
     @Override
     protected String getYahooCSVBasedURL() {
         return "http://download.finance.yahoo.com/d/quotes.csv?s=";
     }
+    */
     
     public YahooStockServer() {
         super();
