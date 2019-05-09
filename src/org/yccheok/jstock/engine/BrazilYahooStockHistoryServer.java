92e71a82ec086827ec738397c408e55a07d003bb
danap <danap@dandymadeproductions.com>
2019-05-09 17:42:01 -0600

    Changed extends AbstractYahooStockHistoryServer too YahooStockHistoryServer2.

--- src/org/yccheok/jstock/engine/BrazilYahooStockHistoryServer.java
+++ src/org/yccheok/jstock/engine/BrazilYahooStockHistoryServer.java
@@ -1,6 +1,9 @@
 /*
  * JStock - Free Stock Market Software
  * Copyright (C) 2009 Yan Cheng CHEOK <yccheok@yahoo.com>
+ * Copyright (C) 2019 Dana Proctor
+ * 
+ * Version 1.0.7.9.01 05/09/2019
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,14 +19,32 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+//=================================================================
+// Revision History
+// Changes to the code should be documented here and reflected
+// in the present version number. Author information should
+// also be included with the original copyright author.
+//=================================================================
+//
+// Version 1.0.7.9    07/21/2015 Original Yan Cheng, JStock Engine BrazilYahooStockHistoryServer
+//                               Class.
+//         1.0.7.9.01 05/09/2019 Changed extends AbstractYahooStockHistoryServer too Yahoo
+//                               StockHistoryServer2.
+//                                
+//-----------------------------------------------------------------
+//                 yccheok@yahoo.com
+//                 danap@dandymadeproductions.com
+//=================================================================
 
 package org.yccheok.jstock.engine;
 
 /**
  *
  * @author yccheok
+ * @author Dana M. Proctor
+ * @version 1.0.7.9.01 05/09/2019
  */
-public class BrazilYahooStockHistoryServer extends AbstractYahooStockHistoryServer {
+public class BrazilYahooStockHistoryServer extends YahooStockHistoryServer2 {
 
     public BrazilYahooStockHistoryServer(Code code) throws StockHistoryNotFoundException
     {
