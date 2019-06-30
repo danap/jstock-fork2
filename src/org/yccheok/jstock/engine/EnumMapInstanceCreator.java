593a249dabcebff83f7cfb3f09e84b3cb5750a00
danap <danap@dandymadeproductions.com>
2019-06-30 14:16:53 -0600

    Original JStock Engine EnumMapInstanceCreator Class. This was a Private Inner Class in gui.Utils. Still Present. Does Not Appear to be Used.

---
+++ src/org/yccheok/jstock/engine/EnumMapInstanceCreator.java
@@ -0,0 +1,59 @@
+/*
+ * JStock-Min - Free Stock Market Software
+ * Copyright (C) 2016 Yan Cheng Cheok <yccheok@yahoo.com>
+ * Copyright (C) 2019 Dana Proctor
+ * 
+ * Version 1.0.7.37.01 06/30/2019
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+//=================================================================
+// Revision History
+// Changes to the code should be documented here and reflected
+// in the present version number. Author information should
+// also be included with the original copyright author.
+//=================================================================
+//
+// Version 1.0.7.37.01 06/26/2019 Original JStock Engine EnumMapInstanceCreator Class.
+//                                This was a Private Inner Class in gui.Utils. Still Present.
+//                                Does Not Appear to be Used.
+//
+//-----------------------------------------------------------------
+//                 danap@dandymadeproductions.com
+//=================================================================
+
+package org.yccheok.jstock.engine;
+
+import java.util.EnumMap;
+
+import com.google.gson.InstanceCreator;
+
+/**
+ *
+ * @author Dana M. Proctor
+ * @version 1.0.7.37.01 06/30/2019
+ */
+public class EnumMapInstanceCreator<K extends Enum<K>, V> implements InstanceCreator<EnumMap<K, V>>
+{
+   private final Class<K> enumClazz;
+   
+   public EnumMapInstanceCreator(final Class<K> enumClazz) {
+      this.enumClazz = enumClazz;
+   }
+   
+   public EnumMap<K, V> createInstance(final java.lang.reflect.Type type) {
+      return new EnumMap<K, V>(enumClazz);
+   }
+}
