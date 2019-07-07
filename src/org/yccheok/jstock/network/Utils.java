2b21befbcd5fb87bea23ffa217c0498f8dc9eca3
danap <danap@dandymadeproductions.com>
2019-07-07 12:50:14 -0600

    Commented Types GET_TIME, GET_IP, & DRIVEWEALTH_LOG. Commented Class Instance JSTOCK_WEBAPP_SERVER.

--- src/org/yccheok/jstock/network/Utils.java
+++ src/org/yccheok/jstock/network/Utils.java
@@ -3,7 +3,7 @@
  * Copyright (C) 2011 Yan Cheng CHEOK <yccheok@yahoo.com>
  * Copyright (C) 2019 Dana Proctor
  * 
- * Version 1.0.7.37.02 02/23/2019
+ * Version 1.0.7.37.03 07/07/2019
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -30,6 +30,8 @@
 //         1.0.7.37.01 02/22/2019 Type, Removed ANDROID_HTML & WENOTE_HTML.
 //         1.0.7.37.01 02/23/2019 Type, Removed HELP_HTML, DONATE_HTML, CONTRIBUTE_HTML,
 //                                & HELP_KEYBOARD_SHORTCUTS_HTML.
+//         1.0.7.37.02 07/07/2019 Commented Types GET_TIME, GET_IP, & DRIVEWEALTH_LOG.
+//                                Commented Class Instance JSTOCK_WEBAPP_SERVER.
 //
 //-----------------------------------------------------------------
 //                 yccheok@yahoo.com
@@ -47,7 +49,7 @@ import org.yccheok.jstock.engine.Country;
  * This class is an utility for network related activities.
  * @author yccheok
  * @author Dana M. Proctor
- * @version 1.0.7.37.02 02/23/2019
+ * @version 1.0.7.37.03 07/07/2019
  */
 public class Utils {
     public enum Type {
@@ -60,12 +62,12 @@ public class Utils {
         HELP_STOCK_DATABASE_HTML,
         PRIVACY_HTML,
         MA_INDICATOR_HTML,
-        GET_TIME,
-        GET_IP,
+        //GET_TIME,
+        //GET_IP,
         OPTIONS,
         STOCK_INFO_DATABASE_META,
         ANDROID_FAQ,
-        DRIVEWEALTH_LOG,
+        //DRIVEWEALTH_LOG,
         GOOGLE_CODE_DATABASE_META,
         GOOGLE_CODE_DATABASE,
         MISSING_STOCK_CODE_SERVICE,
@@ -110,7 +112,7 @@ public class Utils {
 
     //private static final String JSTOCK_STATIC_SERVER = "http://jstock-static-hrd.appspot.com/";
     private static final String JSTOCK_STATIC_SERVER = "https://raw.githubusercontent.com/yccheok/jstock/master/appengine/jstock-static/war/";
-    private static final String JSTOCK_WEBAPP_SERVER = "https://jstock-webapp.appspot.com/";
+    //private static final String JSTOCK_WEBAPP_SERVER = "https://jstock-webapp.appspot.com/";
 
     private static final String COUNTRY_TEMPLATE = "{country}";
     
@@ -124,16 +126,16 @@ public class Utils {
         map.put(Type.HELP_STOCK_DATABASE_HTML, "http://www.jstock.org/help_real_time_info.html?utm_source=jstock&utm_medium=database_dialog#new-database");
         map.put(Type.PRIVACY_HTML, "http://jstock.org/privacy.html");
         map.put(Type.MA_INDICATOR_HTML, "http://jstock.org/ma_indicator.html?utm_source=jstock&utm_medium=chart_dialog");
-        map.put(Type.GET_TIME, JSTOCK_WEBAPP_SERVER + "get-time.py");
-        map.put(Type.GET_IP, JSTOCK_WEBAPP_SERVER + "get-ip.py");
+        //map.put(Type.GET_TIME, JSTOCK_WEBAPP_SERVER + "get-time.py");
+        //map.put(Type.GET_IP, JSTOCK_WEBAPP_SERVER + "get-ip.py");
         map.put(Type.OPTIONS, JSTOCK_STATIC_SERVER + "options_information/options.txt");
         // http://webmasters.stackexchange.com/questions/35413/unable-to-use-anchor-hash-tag-if-using-google-analytics-utm
         map.put(Type.STOCK_INFO_DATABASE_META, JSTOCK_STATIC_SERVER + "stocks_information/stock-info-database-meta.json");
         map.put(Type.ANDROID_FAQ, "http://faq.jstock.co/?utm_source=jstock-android&utm_medium=settings");
-        map.put(Type.DRIVEWEALTH_LOG, "https://jstock-drivewealth.appspot.com/log");
+        //map.put(Type.DRIVEWEALTH_LOG, "https://jstock-drivewealth.appspot.com/log");
         map.put(Type.GOOGLE_CODE_DATABASE_META, JSTOCK_STATIC_SERVER + "stocks_information/google-code-database-meta.json");
         map.put(Type.GOOGLE_CODE_DATABASE, JSTOCK_STATIC_SERVER + "stocks_information/" + COUNTRY_TEMPLATE + "/google-code-database.zip");
-        map.put(Type.MISSING_STOCK_CODE_SERVICE, JSTOCK_WEBAPP_SERVER + "missing-stock-code/");
+        //map.put(Type.MISSING_STOCK_CODE_SERVICE, JSTOCK_WEBAPP_SERVER + "missing-stock-code/");
         map.put(Type.YAHOO_FINANCE_API, "https://mobile-query.finance.yahoo.com/v6/finance/");
         map.put(Type.YAHOO_FINANCE_CHART_API_V8, "https://query1.finance.yahoo.com/v8/finance/chart/");
         map.put(Type.IEX_API, "https://api.iextrading.com/1.0/");
@@ -147,16 +149,16 @@ public class Utils {
         zh_map.put(Type.HELP_STOCK_DATABASE_HTML, "http://jstock.org/zh/help_stock_database.html?utm_source=jstock&utm_medium=database_dialog");
         zh_map.put(Type.PRIVACY_HTML, "http://jstock.org/zh/privacy.html");
         zh_map.put(Type.MA_INDICATOR_HTML, "http://jstock.org/zh/ma_indicator.html?utm_source=jstock&utm_medium=chart_dialog");
-        zh_map.put(Type.GET_TIME, JSTOCK_WEBAPP_SERVER + "get-time.py");
-        zh_map.put(Type.GET_IP, JSTOCK_WEBAPP_SERVER + "get-ip.py");
+        //zh_map.put(Type.GET_TIME, JSTOCK_WEBAPP_SERVER + "get-time.py");
+        //zh_map.put(Type.GET_IP, JSTOCK_WEBAPP_SERVER + "get-ip.py");
         zh_map.put(Type.OPTIONS, JSTOCK_STATIC_SERVER + "options_information/options.txt");
         // http://webmasters.stackexchange.com/questions/35413/unable-to-use-anchor-hash-tag-if-using-google-analytics-utm
         zh_map.put(Type.STOCK_INFO_DATABASE_META, JSTOCK_STATIC_SERVER + "stocks_information/stock-info-database-meta.json");
         zh_map.put(Type.ANDROID_FAQ, "http://faq.jstock.co/zh/?utm_source=jstock-android&utm_medium=settings");
-        zh_map.put(Type.DRIVEWEALTH_LOG, "https://jstock-drivewealth.appspot.com/log");
+        //zh_map.put(Type.DRIVEWEALTH_LOG, "https://jstock-drivewealth.appspot.com/log");
         zh_map.put(Type.GOOGLE_CODE_DATABASE_META, JSTOCK_STATIC_SERVER + "stocks_information/google-code-database-meta.json");
         zh_map.put(Type.GOOGLE_CODE_DATABASE, JSTOCK_STATIC_SERVER + "stocks_information/" + COUNTRY_TEMPLATE + "/google-code-database.zip");
-        zh_map.put(Type.MISSING_STOCK_CODE_SERVICE, JSTOCK_WEBAPP_SERVER + "missing-stock-code/");
+        //zh_map.put(Type.MISSING_STOCK_CODE_SERVICE, JSTOCK_WEBAPP_SERVER + "missing-stock-code/");
         zh_map.put(Type.YAHOO_FINANCE_API, "https://mobile-query.finance.yahoo.com/v6/finance/");
         zh_map.put(Type.YAHOO_FINANCE_CHART_API_V8, "https://query1.finance.yahoo.com/v8/finance/chart/");
         zh_map.put(Type.IEX_API, "https://api.iextrading.com/1.0/");
@@ -170,16 +172,16 @@ public class Utils {
         zh_Hant_map.put(Type.HELP_STOCK_DATABASE_HTML, "http://jstock.org/zh/help_stock_database.html?utm_source=jstock&utm_medium=database_dialog");
         zh_Hant_map.put(Type.PRIVACY_HTML, "http://jstock.org/zh/privacy.html");
         zh_Hant_map.put(Type.MA_INDICATOR_HTML, "http://jstock.org/zh/ma_indicator.html?utm_source=jstock&utm_medium=chart_dialog");
-        zh_Hant_map.put(Type.GET_TIME, JSTOCK_WEBAPP_SERVER + "get-time.py");
-        zh_Hant_map.put(Type.GET_IP, JSTOCK_WEBAPP_SERVER + "get-ip.py");
+        //zh_Hant_map.put(Type.GET_TIME, JSTOCK_WEBAPP_SERVER + "get-time.py");
+        //zh_Hant_map.put(Type.GET_IP, JSTOCK_WEBAPP_SERVER + "get-ip.py");
         zh_Hant_map.put(Type.OPTIONS, JSTOCK_STATIC_SERVER + "options_information/options.txt");
         // http://webmasters.stackexchange.com/questions/35413/unable-to-use-anchor-hash-tag-if-using-google-analytics-utm
         zh_Hant_map.put(Type.STOCK_INFO_DATABASE_META, JSTOCK_STATIC_SERVER + "stocks_information/stock-info-database-meta.json");
         zh_Hant_map.put(Type.ANDROID_FAQ, "http://faq.jstock.co/zh-Hant/?utm_source=jstock-android&utm_medium=settings");
-        zh_Hant_map.put(Type.DRIVEWEALTH_LOG, "https://jstock-drivewealth.appspot.com/log");
+        //zh_Hant_map.put(Type.DRIVEWEALTH_LOG, "https://jstock-drivewealth.appspot.com/log");
         zh_Hant_map.put(Type.GOOGLE_CODE_DATABASE_META, JSTOCK_STATIC_SERVER + "stocks_information/google-code-database-meta.json");
         zh_Hant_map.put(Type.GOOGLE_CODE_DATABASE, JSTOCK_STATIC_SERVER + "stocks_information/" + COUNTRY_TEMPLATE + "/google-code-database.zip");
-        zh_Hant_map.put(Type.MISSING_STOCK_CODE_SERVICE, JSTOCK_WEBAPP_SERVER + "missing-stock-code/");
+        //zh_Hant_map.put(Type.MISSING_STOCK_CODE_SERVICE, JSTOCK_WEBAPP_SERVER + "missing-stock-code/");
         zh_Hant_map.put(Type.YAHOO_FINANCE_API, "https://mobile-query.finance.yahoo.com/v6/finance/");
         zh_Hant_map.put(Type.YAHOO_FINANCE_CHART_API_V8, "https://query1.finance.yahoo.com/v8/finance/chart/");
         zh_Hant_map.put(Type.IEX_API, "https://api.iextrading.com/1.0/");
